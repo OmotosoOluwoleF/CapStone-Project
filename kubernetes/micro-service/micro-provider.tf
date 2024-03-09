9cd7f2fef0661d60e9f390b081dc0b928a4799e2
OmotosoOluwoleF <omotosooluwolefolorunso@gmail.com>
2024-03-09 21:13:34 +0000

    first commit

--- kubernetes/micro-service/micro-provider.tf
+++ kubernetes/micro-service/micro-provider.tf
@@ -13,10 +13,11 @@ data "aws_eks_cluster" "pm-dev-eks-project" {
 data "aws_eks_cluster_auth" "pm-dev-eks-project_auth" {
   name = "pm-dev-eks-project_auth"
 }
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.pm-dev-eks-project.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.pm-dev-eks-project.certificate_authority[0].data)
-  version          = "2.16.1"
+  version        = "2.16.1"
   config_path = "~/.kube/config"
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
